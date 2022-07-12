6ffff9529b9ed7dba1c6e300dcbc7df7840a5eab
raialb <ralbino@hawk.iit.edu>
2022-07-12 13:06:51 -0500

    To-Do App in React JS

---
+++ src/components/todolist.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Todo from "./todo"; // inserting components
+
+
+const Todolist = ({todos, setTodos, filteredTodos}) => {
+    return (
+        <div className="todo-container">
+            <ul className="todo-list">
+                {filteredTodos.map((todo) => (
+                <Todo setTodos={setTodos} todos={todos} todo ={todo}
+                key ={todo.id} text={todo.text} />
+            ))}
+            </ul>
+        </div>
+    ); 
+}
+
+export default Todolist;
